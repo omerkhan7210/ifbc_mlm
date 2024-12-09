7eff9492accf0efcf2316ec1597c9d74619d7c8d
sohailahmad-dev <sohailahmaddev@gmail.com>
2024-12-09 22:17:31 +0500

    added members pg in userManagement

---
+++ src/configs/routes.config/userManagementRoutes.ts
@@ -0,0 +1,12 @@
+import { lazy } from 'react'
+import type { Routes } from '@/@types/routes'
+import { USER_MANAGeMENT_PREFIX_PATH } from '@/constants/route.constant'
+
+export const userManagementRoutes: Routes = [
+    {
+        key: 'members',
+        path: `${USER_MANAGeMENT_PREFIX_PATH}/members`,
+        component: lazy(() => import('@/views/userManagement/members')),
+        authority: [],
+    },
+]
